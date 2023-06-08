241d7ce4f2de3d6648e2070074e9bb11a2399568
Artefilex <baris.tncdmr@gmail.com>
2023-06-08 22:01:53 +0300

    jquery first

---
+++ javascript/jquery/js/main.js
@@ -0,0 +1,11 @@
+// external js kullanılıyorsa document ready kullanmamız lazım 
+
+$(document).ready(()=>{
+    $('button').click(() => {
+        // $("#jquery").hide()
+        // $(".blabla").hide()
+          $("p").hide()
+       })
+       
+});
+
