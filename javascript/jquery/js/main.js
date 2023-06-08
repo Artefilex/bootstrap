0b4457e4b43e93f7b86868848219506625d9f588
Artefilex <baris.tncdmr@gmail.com>
2023-06-08 22:55:25 +0300

    first work in jquery addClass removeClass css

--- javascript/jquery/js/main.js
+++ javascript/jquery/js/main.js
@@ -1,11 +1,45 @@
 // external js kullanılıyorsa document ready kullanmamız lazım 
 
-$(document).ready(()=>{
-    $('button').click(() => {
-        // $("#jquery").hide()
-        // $(".blabla").hide()
-          $("p").hide()
-       })
+// $(document).ready(()=>{
+//     $('button').click(() => {
+//         // $("#jquery").hide()
+//         $(".blabla").css("color", "red" )
+//         //   $("p").hide()
+//        })
        
-});
+//     //  $(":button").click()   tüm buttonları secer 
+//     // $(":checkbox") tüm checkboxlar
+//     //  $(":header") tüm h etşketlerini secer 
+//     // $(":empty") boş olan divleri secer 
+//     // :first :last 
+//     // :input
+//     // :checked :password :reset :odd :even :focus : image 
+//     //  : contains()  $("p:contains(deneme))
+//     //  :has()
+//     // :not()
 
+//     //  , ile bircok seyi ayırıp aynı seyleri verebiliyoryz 
+// });
+
+$(document).ready(()=>{
+    $("body").css("background" , "rgb(156,156,156)")
+    $("table").css({"background" : "black", "color": "white" })
+    let objCss = {
+        "backgound": "black",
+        "color": "white",
+        "width": "75%",
+        "margin": "auto",
+        "margin-top": "1rem"
+    }
+    
+    $("table").css(objCss)
+        $("input").focus(function() {
+            $(this).addClass("focused");
+        });
+
+        $("input").blur(function() {
+            $(this).removeClass("focused");
+        });
+
+   
+})
