fe8dfd387f00c64a3ed9fa7d5097a376a1b6c395
Artefilex <baris.tncdmr@gmail.com>
2023-06-10 00:09:38 +0300

    methods

---
+++ javascript/jquery/js/methods.js
@@ -0,0 +1,61 @@
+// let fx = ()=>{
+//     $.fx.off= !$.fx.off
+// }
+//  bu fx.off degiskenini bir classa id click eventi olarak verince animasyonlarda toogle özelligi görür
+// yani çalışıyorsa durdurur calısmıyorsa calıstırır daha dogrusu animasyton yapıları durdurr ya da acar 
+
+// trim()
+// index()
+// find()
+
+
+// $(document).ready(()=>{
+//     // $("p").after("cnjdjdddj")
+
+//     $("li").click(()=>{
+//         // let indis = $(this).index()
+//     //   let aftering = $(this).after("<b> bla bla bla  </b>") not work 
+//         // alert(indis)
+//         //  $("p").text(indis)
+//         // alert(aftering)  not work 
+//     })
+// })
+
+
+
+// clone()
+// detach() kaldırmak için kullanılır 
+// on( )
+
+// $(document).ready(()=>{
+//     // $("button").click(()=>{
+//     // //   $("p").clone().appendTo("#append")
+//     // //    $("p").detach()
+
+
+//     // })
+
+//     // $("button").on({
+//     //     mouseenter:()=>{
+//     //         alert("maouseenter")
+//     //     },
+//     //     mouseleave:()=>{
+//     //         alert("mouseleave")
+//     //     },
+//     //     click:()=>{
+//     //         alert("click")
+//     //     }
+//     // })
+// })
+
+// parant() parants() sibling() chidren() find() next() nextAll() nextUtil()  prev() prevAll() prevUtil()
+
+
+$(document).ready(()=>{
+    // $("#ptag").parent().css("background" , "black").css("padding","1rem")
+    // $("#ptag").parents().css("background" , "red").css("padding","1rem")
+    // $(".div").children().css("color" , "black").css("padding","1rem")
+    // $("li").siblings().css("color" , "blue").css("padding","1rem")
+    //  $("ul").find("li").css("color" , "black").css("padding","1rem")
+
+})
