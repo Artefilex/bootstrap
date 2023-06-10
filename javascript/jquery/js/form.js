9e59bdbef50ed7b1b2b99165821fdcc2096a0a84
Artefilex <baris.tncdmr@gmail.com>
2023-06-10 10:25:24 +0300

    form validation

---
+++ javascript/jquery/js/form.js
@@ -0,0 +1,33 @@
+$(document).ready(()=>{
+//    $("#name").focus(()=>{
+//     $(".btn").css("background", "black").fadeOut(1500)
+//    })
+//    $("#surname").blur(()=>{
+//     $(".btn").css("background","red").fadeIn(1000)
+//    })
+
+//    $("form").submit((e)=>{
+//     e.preventDefault()
+//     let val1 = $("#name").val()
+//     let val2 = $("#surname").val()
+//   if(val1 !== null && val2 !== null){
+//     $(".user").after(val1 + " "+ val2)
+//     val1 = $("#name").val("")
+//      val2 = $("#surname").val("")
+   
+//   }
+//     })
+
+//  append() sona eklemek after()     prepend() önce eklemek before()
+// remove() kaldır  empty() childleri kaldırır
+
+// $(".user ul").append("<li> append() </li>")
+// $(".user ul").after("<li> after() </li>")
+// $(".user ul").prepend("<li> prepend() </li>")
+// $(".user ul").before("<li> before() </li>")
+// $("ul li").remove()
+// $("ul li").empty()
+})
+
+
+
